76f452a3f1582278ebf0c5c02a104545bce840d5
Jeff McRiffey <jmcriffey@gmail.com>
2016-12-08 13:33:34 -0500

    add unit tests for new features

--- test/spec/index.spec.js
+++ test/spec/index.spec.js
@@ -9,8 +9,8 @@ describe("Test electrode-react-webapp", () => {
   let config;
   let paths;
 
-  beforeEach(() => {
-    config = {
+  const getConfig = () => {
+    return {
       plugins: {
         "./lib/index": {
           options: {
@@ -29,9 +29,11 @@ describe("Test electrode-react-webapp", () => {
         }
       }
     };
+  };
 
+  beforeEach(() => {
+    config = getConfig();
     paths = config.plugins["./lib/index"].options.paths["/{args*}"];
-
   });
 
   const stopServer = (server) =>
@@ -104,6 +106,152 @@ describe("Test electrode-react-webapp", () => {
         }).then((res) => {
           expect(res.statusCode).to.equal(500);
           expect(res.result).to.equal("Internal server error");
+          stopServer(server);
+        })
+        .catch((err) => {
+          stopServer(server);
+          throw err;
+        });
+      });
+  });
+
+  it("should handle multiple entry points - foo", () => {
+    const serverConfig = getConfig();
+    const serverOptions = serverConfig.plugins["./lib/index"].options;
+
+    serverOptions.bundleChunkSelector = "test/data/chunk-selector.js";
+    serverOptions.stats = "test/data/stats-test-multibundle.json";
+
+    return electrodeServer(serverConfig)
+      .then((server) => {
+        return server.inject({
+          method: "GET",
+          url: "/foo"
+        }).then((res) => {
+          expect(res.statusCode).to.equal(200);
+          expect(res.result).to.contain("foo.bundle.f07a873ce87fc904a6a5.js");
+          expect(res.result).to.contain("foo.style.f07a873ce87fc904a6a5.css");
+          stopServer(server);
+        })
+        .catch((err) => {
+          stopServer(server);
+          throw err;
+        });
+      });
+  });
+
+  it("should handle multiple entry points - bar", () => {
+    const serverConfig = getConfig();
+    const serverOptions = serverConfig.plugins["./lib/index"].options;
+
+    serverOptions.bundleChunkSelector = "test/data/chunk-selector.js";
+    serverOptions.stats = "test/data/stats-test-multibundle.json";
+
+    return electrodeServer(serverConfig)
+      .then((server) => {
+        return server.inject({
+          method: "GET",
+          url: "/bar"
+        }).then((res) => {
+          expect(res.statusCode).to.equal(200);
+          expect(res.result).to.contain("bar.bundle.f07a873ce87fc904a6a5.js");
+          expect(res.result).to.contain("bar.style.f07a873ce87fc904a6a5.css");
+          stopServer(server);
+        })
+        .catch((err) => {
+          stopServer(server);
+          throw err;
+        });
+      });
+  });
+
+  it("should handle multiple entry points - default", () => {
+    const serverConfig = getConfig();
+    const serverOptions = serverConfig.plugins["./lib/index"].options;
+
+    serverOptions.bundleChunkSelector = "test/data/chunk-selector.js";
+    serverOptions.stats = "test/data/stats-test-multibundle.json";
+
+    return electrodeServer(serverConfig)
+      .then((server) => {
+        return server.inject({
+          method: "GET",
+          url: "/"
+        }).then((res) => {
+          expect(res.statusCode).to.equal(200);
+          expect(res.result).to.contain("bundle.f07a873ce87fc904a6a5.js");
+          expect(res.result).to.contain("style.f07a873ce87fc904a6a5.css");
+          stopServer(server);
+        })
+        .catch((err) => {
+          stopServer(server);
+          throw err;
+        });
+      });
+  });
+
+  it("should inject a pwa manfiest", () => {
+    const serverConfig = getConfig();
+    const serverOptions = serverConfig.plugins["./lib/index"].options;
+
+    serverOptions.stats = "test/data/stats-test-pwa.json";
+
+    return electrodeServer(serverConfig)
+      .then((server) => {
+        return server.inject({
+          method: "GET",
+          url: "/"
+        }).then((res) => {
+          expect(res.statusCode).to.equal(200);
+          expect(res.result).to.contain("<link rel=\"manifest\" href=\"/js/manifest.json\" />");
+          stopServer(server);
+        })
+        .catch((err) => {
+          stopServer(server);
+          throw err;
+        });
+      });
+  });
+
+  it("should inject pwa icons", () => {
+    const serverConfig = getConfig();
+    const serverOptions = serverConfig.plugins["./lib/index"].options;
+
+    serverOptions.iconStats = "test/data/icon-stats-test-pwa.json";
+
+    return electrodeServer(serverConfig)
+      .then((server) => {
+        return server.inject({
+          method: "GET",
+          url: "/"
+        }).then((res) => {
+          expect(res.statusCode).to.equal(200);
+          expect(res.result).to.contain("<meta name=\"mobile-web-app-capable\" content=\"yes\">");
+          expect(res.result).to.contain("<meta name=\"application-name\" content=\"Electrode\">");
+          expect(res.result).to.contain("<link rel=\"icon\" href=\"/js/favicon-32x32.png\">");
+          stopServer(server);
+        })
+        .catch((err) => {
+          stopServer(server);
+          throw err;
+        });
+      });
+  });
+
+  it("should inject critical css", () => {
+    const serverConfig = getConfig();
+    const serverOptions = serverConfig.plugins["./lib/index"].options;
+
+    serverOptions.criticalCSS = "test/data/critical.css";
+
+    return electrodeServer(serverConfig)
+      .then((server) => {
+        return server.inject({
+          method: "GET",
+          url: "/"
+        }).then((res) => {
+          expect(res.statusCode).to.equal(200);
+          expect(res.result).to.contain("<style>body {color: green;}\n</style>");
           stopServer(server);
         })
         .catch((err) => {
